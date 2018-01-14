2763c5c865c1dc1e376cfb283424bdd7b81e3d04
Matthias Rude <matze.development@gmail.com>
2018-01-14 14:48:47 +0100

    activate delete function and add test

--- src/test/java/edu/hm/sweI/eam/controller/ActivityControllerTest.java
+++ src/test/java/edu/hm/sweI/eam/controller/ActivityControllerTest.java
@@ -1,15 +1,22 @@
 package edu.hm.sweI.eam.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
 import static edu.hm.sweI.eam.Constants.API_BASE;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class ActivityControllerTest {
 
+    private String testActivityName = "TestActivity923478523940237529304";
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -29,6 +38,46 @@ public class ActivityControllerTest {
     public void noParamActivityShouldReturnActivityList() throws Exception {
         this.mockMvc.perform(get(API_BASE + "/activity")).andDo(print()).andExpect(status().isOk());
 //                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void addActivity() throws Exception {
+        MockHttpServletResponse response = this.mockMvc.perform(
+                post(API_BASE + "/activity")
+                        .content("{\"title\":\"" + testActivityName + "\",\"text\":\"test test\",\"tags\":[]}")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        int id = extractID(response);
+        mockMvc.perform(delete(API_BASE + "/activity/" + id));
+    }
+
+    @Test
+    public void deleteActivity() throws Exception {
+        MockHttpServletResponse response = this.mockMvc.perform(
+                post(API_BASE + "/activity")
+                        .content("{\"title\":\"" + testActivityName + "\",\"text\":\"test test\",\"tags\":[]}")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        int id = extractID(response);
+        mockMvc.perform(delete(API_BASE + "/activity/" + id)).andExpect(status().isOk());
+    }
+
+
+    private int extractID(MockHttpServletResponse response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode content = mapper.readTree(response.getContentAsString());
+        assertNotNull(content);
+        assertNotNull(content.findValue("id"));
+
+        return content.findValue("id").asInt();
     }
 }
 
